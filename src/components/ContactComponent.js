5564fdc15775d1149fd2d6156354782331ce99ee
lincolndu <lincolndu@yahoo.com>
2020-06-08 12:52:28 +0000

    react redux

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -68,13 +68,14 @@ class Contact extends Component {
     	}else if(this.state.touched.lastname && lastname.length > 10){
     		errors.lastname="Last Name should be <=10 characters";
     	}
-    	const reg= /^\d$/;
+    	const reg= /^\d+$/;
     	if (this.state.touched.telnum && !reg.test(telnum) ){
     		errors.telnum="Tel. Number should contain only";
     	}
-    	if (this.state.touched.email && email.split('').filter(x=>x==='@').length !==1 ) {
+    	if (this.state.touched.email && email.split('').filter(x=>x==='@').length !== 1 ) {
     		errors.email="Email should contain a @";
     	}
+
     	return errors;
     }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,27 +3,24 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import {Switch,Route,Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
+
+
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
 
 class Main extends Component { 
-	constructor(props){
-		super(props);
-		this.state = {
-	      dishes: DISHES,
-	      comments: COMMENTS,
-	      promotions: PROMOTIONS,
-	      leaders: LEADERS
-	    };
-	}
 
-	
 
 	render(){
 		const HomePage=(dishes,promotions,leaders)=>{
@@ -37,8 +34,8 @@ class Main extends Component {
 		}
 		const DishWithId = ({match}) => {
 	      return(
-	          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-	            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+	          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+	            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
 	      );
 	    };
 
@@ -46,11 +43,11 @@ class Main extends Component {
 			<div className="App">
 			  <Header />
 				  <Switch>
-				  	<Route path="/home" component={()=>HomePage(this.state.dishes,this.state.promotions,this.state.leaders)} />
-				  	<Route exact path="/menu" component={()=><Menu dishes={this.state.dishes} />} />
+				  	<Route path="/home" component={()=>HomePage(this.props.dishes,this.props.promotions,this.props.leaders)} />
+				  	<Route exact path="/menu" component={()=><Menu dishes={this.props.dishes} />} />
 				  	<Route path='/menu/:dishId' component={DishWithId} />
 				  	<Route path="/contactus" component={Contact} />
-				  	<Route path="/aboutus" component={()=><About leaders={this.state.leaders} />} />
+				  	<Route path="/aboutus" component={()=><About leaders={this.props.leaders} />} />
 				  	<Redirect to="home" />
 				  </Switch>
 			  <Footer />
@@ -58,4 +55,4 @@ class Main extends Component {
 		); 
 	}	
 }
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
